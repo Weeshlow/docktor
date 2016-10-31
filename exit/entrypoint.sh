ffb3a230233baf700cfc0419a9a079677db44813
Beard <security@kernel.is>
2016-10-31 00:27:45 -0700

    Update entrypoint.sh

--- exit/entrypoint.sh
+++ exit/entrypoint.sh
@@ -2,11 +2,11 @@
 #===============================================================================================================================================
 # (C) Copyright 2016 TorWorld (https://torworld.org) a project under the CryptoWorld Foundation (https://cryptoworld.is).
 #
-# Licensed under the GNU GENERAL PUBLIC LICENSE, Version 3.0 (the "License");
+# Licensed under the GNU AFFERO GENERAL PUBLIC LICENSE, Version 3 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#     https://www.gnu.org/licenses/gpl-3.0.en.html
+#     https://www.gnu.org/licenses/agpl-3.0.en.html
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
